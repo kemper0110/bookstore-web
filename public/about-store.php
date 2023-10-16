6c44f85a3859389d7354edf54576c295845d14ed
admin <admin@work.com>
2023-10-16 13:08:36 +0300

    make view as html

--- public/about-store.php
+++ public/about-store.php
@@ -1,2 +1,7 @@
 <?php
-require "../views/about-store.view.php";
+
+require "partials/header.view.php";
+require "partials/nav.view.php";
+readfile('../views/about-store.view.html');
+
+require "partials/footer.view.php";
