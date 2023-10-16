211d4dc8f1bdbf6a75c5df707ef25a17bba4b50f
admin <admin@work.com>
2023-10-16 13:09:48 +0300

    about-store: make view as html

--- public/admin/update-about-store.php
+++ public/admin/update-about-store.php
@@ -2,14 +2,14 @@
 require "authenticated.php";
 
 handle_page_update(
-    '../../views/about-store.view.php',
-    'views/about-store.view.php',
+    '../../views/about-store.view.html',
+    'views/about-store.view.html',
     '/admin/update-about-store.php'
 );
 
 $editor = [
-    'filename' => 'about-store.view.php',
-    'code' => file_get_contents('../../views/about-store.view.php'),
+    'filename' => 'about-store.view.html',
+    'code' => file_get_contents('../../views/about-store.view.html'),
     'href' => '/about-store.php'
 ];
 
