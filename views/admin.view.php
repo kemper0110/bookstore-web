440a0a444df048b4b6efd280e9a06d56263684d5
admin <admin@work.com>
2023-10-16 20:30:51 +0300

    removed updated page links from admin

--- views/admin.view.php
+++ views/admin.view.php
@@ -1,11 +1,14 @@
 <?php require "partials/header.view.php" ?>
 
 <section class="mt-8 w-full flex flex-col gap-8 place-items-center text-2xl font-medium">
-    <a href="/admin/update-main.php">
-        Редактирование главной страницы
-    </a>
-    <a href="/admin/update-detailed.php">
-        Редактирование подробной страницы
+<!--    <a href="/admin/update-main.php">-->
+<!--        Редактирование главной страницы-->
+<!--    </a>-->
+<!--    <a href="/admin/update-detailed.php">-->
+<!--        Редактирование подробной страницы-->
+<!--    </a>-->
+    <a href="/admin/update-about-me.php">
+        Редактирование страницы об авторе
     </a>
     <a href="/admin/update-about-store.php">
         Редактирование страницы о магазине
